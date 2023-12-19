c1375e0f33f41d2a321c5174c24b3f1580715912
vvvvss <songweiyi0318@gmail.com>
2023-12-19 16:06:03 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+function App() {
+
+  const [catFact, setCatFact] = useState("")
+  useEffect(() => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      console.log(res.data);
+      setCatFact(res.data.fact);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      {catFact}
+    </div>
+  );
+}
+
+export default App;
